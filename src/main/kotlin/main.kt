1cc5a0b542b6bee2315bbd713087126e5fdcaf18
BambooSteel610 <thiendaothucan610@gmail.com>
2021-05-11 18:00:45 +0700

    update function for main file

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -20,8 +20,17 @@ fun output(inputNumbers: List<Int>){
 
 fun calculator(function: (List<Int>) -> Double, numberArg:List<Int>) = function(numberArg)
 
-fun add(numbers:List<Int>):Double{
-    var sum = 0
-    numbers.forEach{sum += it}
-    return sum.toDouble()
+fun LogaritNepe(n:Int):Double{
+    var sum:Double = 0.0
+    var base:Double = 0.0
+    var divisor:Double = 0.0
+    var pow:Double = 0.0
+    base = ((n-1) / (n+1)).toDouble()
+    for(i in 1..10000){
+        divisor = ((2 * i) - 1).toDouble()
+        pow = Math.pow(base,divisor)
+        pow = pow / divisor
+        sum = sum + pow
+    }
+    return 2*sum
 }
